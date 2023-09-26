2aaa9d49183f131746477183e207e3426854875f
K-Takanori <tabo.25.apr.1984@gmail.com>
2023-09-26 19:40:31 +0900

    [fix] 環境変数

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env:{
+    NEXT_PUBLIC_REGION: process.env.NEXT_PUBLIC_REGION,
+    NEXT_PUBLIC_USERPOOLID: process.env.NEXT_PUBLIC_USERPOOLID,
+    NEXT_PUBLIC_USERPOOLWEBCLIENTID: process.env.NEXT_PUBLIC_USERPOOLWEBCLIENTID,
+    NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
+    NEXT_PUBLIC_REDIRECTURI: process.env.NEXT_PUBLIC_REDIRECTURI,
+    NEXT_PUBLIC_APIURL: process.env.NEXT_PUBLIC_APIURL,
+    NEXT_PUBLIC_CUSTOMPROVIDER: process.env.NEXT_PUBLIC_CUSTOMPROVIDER,
+  },
   reactStrictMode: true,
 }
 
