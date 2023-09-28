2b7604d2b41826556e4fa5579fa03598cc2e5c64
K-Takanori <tabo.25.apr.1984@gmail.com>
2023-09-28 21:25:36 +0900

    [fix] sessionまわり

--- hooks/useSessionState.ts
+++ hooks/useSessionState.ts
@@ -1,21 +1,25 @@
 import { useEffect, useState } from "react";
 import { Auth, Hub } from "aws-amplify";
-import { cookies } from 'next/headers'
+import { cookies } from "next/headers";
 import { CognitoUserSession } from "amazon-cognito-identity-js";
 
 export const useSessionState = () => {
-  const [sessionData, setSessionData] = useState<CognitoUserSession | undefined>();
+  const [sessionData, setSessionData] = useState<
+    CognitoUserSession | undefined
+  >();
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     (async () => {
-      const userInfo = await Auth.currentUserInfo();
-      if (userInfo) {
-        const session = await Auth.currentSession();
-        setSessionData(session);
-      } else {
-        setSessionData(undefined);
-      }
+      // const userInfo = await Auth.currentUserInfo().catch((e) => false);
+      // if (userInfo) {
+      //   const session = await Auth.currentSession();
+      //   setSessionData(session);
+      // } else {
+      //   setSessionData(undefined);
+      // }
+      const session = await Auth.currentSession();
+      setSessionData(session);
       setIsLoading(false);
     })();
   }, []);
