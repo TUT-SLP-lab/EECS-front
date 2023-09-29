ba0d27de739eaa543f3dbacdf40cdf116bf026c8
Hansyo <hansyo.souti.1999@gmail.com>
2023-09-30 02:14:40 +0900

    update: タブスタイルに変更

--- components/layouts/MainView.tsx
+++ components/layouts/MainView.tsx
@@ -1,22 +1,30 @@
 import { useModalState } from "@/hooks/useModalState";
-import { useState } from "react";
-import Modal from "../common/modal/modal";
-import Room from "../room";
+import { useEffect, useState } from "react";
+import Room from "../canvas-object/Room";
 import { useDeskState } from "@/hooks/useDeskState";
+import Modal from "../canvas-object/modal/Modal";
+import { Tabs, TabList, Tab, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 export const MainView = () => {
   const { isModalOpen, openModal, closeModal } = useModalState();
   const [changeDeskID, setChangeDeskID] = useState("");
-  const { deskDatas, changeSitDesk, changeStandDesk } =
-    useDeskState();
-  const [roomID, setRoomID] = useState("f-301");
+  const { deskDatas, changeSitDesk, changeStandDesk } = useDeskState();
+  const [selectedTab, setSelectedTab] = useState(0);
 
   const targetDesk = (desk_id: string) => {
     setChangeDeskID(desk_id);
   };
-  const handleClick = (e: any) => {
-    setRoomID(e.target.value);
+
+  const handleTabSelect = (index: number) => {
+    setSelectedTab(index);
+    localStorage.setItem("selectedTab", index.toString());
   };
+
+  useEffect(() => {
+    const storedTab = localStorage.getItem("selectedTab");
+    setSelectedTab(parseInt(storedTab ?? "0"));
+  }, []);
 
   return (
     <div className="p-1">
@@ -27,30 +35,36 @@ export const MainView = () => {
         changeDeskID={changeDeskID}
         changeSitDesk={changeSitDesk}
       />
-      <button
-        type="submit"
-        onClick={handleClick}
-        className="bg-gray-300 flex-1 rounded m-1 p-1"
-        value={"f-301"}
-      >
-        F-301
-      </button>
-      <button
-        type="submit"
-        onClick={handleClick}
-        className="bg-gray-300 flex-1 rounded m-1 p-1"
-        value={"f-310"}
-      >
-        F-310
-      </button>
-      <Room
-        roomNumber={roomID}
-        deskDatas={deskDatas}
-        changeSitDesk={changeSitDesk}
-        changeStandDesk={changeStandDesk}
-        openModal={openModal}
-        targetDesk={targetDesk}
-      />
+      <Tabs selectedIndex={selectedTab} onSelect={handleTabSelect}>
+        <TabList>
+          <Tab>F-301</Tab>
+          <Tab>F-310</Tab>
+        </TabList>
+        <TabPanel>
+          <Room
+            roomNumber={"f-301"}
+            width={640}
+            height={740}
+            deskDatas={deskDatas}
+            changeSitDesk={changeSitDesk}
+            changeStandDesk={changeStandDesk}
+            openModal={openModal}
+            targetDesk={targetDesk}
+          />
+        </TabPanel>
+        <TabPanel>
+          <Room
+            roomNumber={"f-310"}
+            width={650}
+            height={750}
+            deskDatas={deskDatas}
+            changeSitDesk={changeSitDesk}
+            changeStandDesk={changeStandDesk}
+            openModal={openModal}
+            targetDesk={targetDesk}
+          />
+        </TabPanel>
+      </Tabs>
     </div>
   );
 };
